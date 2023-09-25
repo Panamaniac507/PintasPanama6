d854f657544e918d32a408ab61cdb10cce1d7025
Joseph Herbert <herbert.jr80@gmail.com>
2023-09-25 16:16:09 +0900

    models-routes-controller-update

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,51 @@
+class OrdersController < ApplicationController
+  before_action :set_order, only: %i[show update destroy]
+
+  def index
+    @orders = Order.all
+  end
+
+  def show
+    # @article = Article.find(params[:id])
+  end
+
+  def new
+    @order = Order.new
+    @current_user = current_user
+  end
+
+  def create
+    @order = Order.new(order_params)
+    @order.user = current_user
+    if @order.save
+      redirect_to orders_path(@user)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    # @article = Article.find(params[:id])
+    if @order.update(order_params)
+      redirect_to order_path(@order)
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    # @article = Article.find(params[:id])
+    @order.destroy
+    redirect_to orders_path(@orders), status: :see_other
+  end
+
+  private
+
+  def set_order
+    @order = Order.find(params[:id])
+  end
+
+  def order_params
+    params.require(:order).permit(:sub_total, :shipping, :tax, :address, :name, :last_name, :mobile, :age, :status)
+  end
+end
